ab2428562015461574e33334d0f2a20fd3f6ee53
Mike Chen <517926804@qq.com>
2023-12-08 14:47:34 +0800

    Add savehist configuration

--- lisp/init-tweaks.el
+++ lisp/init-tweaks.el
@@ -30,5 +30,25 @@
         gcmh-auto-idle-delay-factor 10
         gcmh-high-cons-threshold (* 16 1024 1024))) ; 16MB
 
+(use-package savehist
+  :hook (after-init . savehist-mode)
+  :init (setq enable-recursive-minibuffers t ; Allow commands in minibuffers
+              history-length 1000
+              savehist-additional-variables '(kill-ring
+                                              mark-ring
+                                              global-mark-ring
+                                              search-ring
+                                              regexp-search-ring
+                                              extended-command-history)
+              savehist-autosave-interval 300)
+  :config
+  (defun bc/savehist-unpropertize-variables-h ()
+    "Remove text properties from `kill-ring' to reduce savehist cache size."
+    (setq kill-ring
+          (mapcar #'substring-no-properties
+                  (cl-remove-if-not #'stringp kill-ring))))
+
+  (add-hook 'savehist-save-hook #'bc/savehist-unpropertize-variables-h))
+
 (provide 'init-tweaks)
 ;;; init-tweaks.el ends here
